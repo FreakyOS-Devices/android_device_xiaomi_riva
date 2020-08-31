f62a0973ab12b870abf1647b82ae1282ddb74a0a
Uptownboys32 <anggaalfian133@gmail.com>
2020-08-31 06:48:41 +0000

    riva: Bring to Freak

--- freaky_riva.mk
+++ freaky_riva.mk
@@ -21,7 +21,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
 
 # Inherit some common AOSP stuff
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/freaky/config/common_full_phone.mk)
+$(call inherit-product, vendor/freaky/config/common.mk)
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1280
@@ -35,7 +36,7 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := riva
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := Redmi 5A
-PRODUCT_NAME := aosp_riva
+PRODUCT_NAME := freaky_riva
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
